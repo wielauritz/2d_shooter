d98ddeb3f07a14607065ac390ddc56a93aa632b2
Drogelbecher <131149172+Drogelbecher@users.noreply.github.com>
2023-04-28 12:05:56 +0200

    Mouseinput (nicht fertig)

--- handlers/MouseInput.java
+++ handlers/MouseInput.java
@@ -1,4 +1,12 @@
 package handlers;
 
-public class MouseInput {
+public class MouseInput extends Window{
+
+PointerInfo a = MouseInfo.getPointerInfo();
+Point b = a.getLocation();
+int x = (int) b.getX();
+int y = (int) b.getY();
+Robot r = new Robot();
+r.mouseMove(x, y - 50);
+
 }
