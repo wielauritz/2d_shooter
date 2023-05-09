024c1cbfa892e4ba2a1984df4a016955884570c7
WWWIEBUSCH <socialmedia@wwwiebusch.de>
2023-05-09 12:28:25 +0200

    idk what i did but i did something

--- entities/Projectile.java
+++ entities/Projectile.java
@@ -1,7 +1,6 @@
 package entities;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Projectile {
 
--- handlers/KeyboardInput.java
+++ handlers/KeyboardInput.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.image.ImageObserver;
 import java.util.HashSet;
 import java.util.Set;
 
